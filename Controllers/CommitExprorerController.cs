bfca2ed0d3ff67484cc5d1ca7a065d198c7e7d48
Jonnier Teran <jonnierteranmorales@gmail.com>
2024-02-02 17:04:30 -0500

    Configuracion Del Api controller

--- Controllers/CommitExprorerController.cs
+++ Controllers/CommitExprorerController.cs
@@ -1,6 +1,21 @@
-﻿namespace ExploradorCommitsApp.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExploradorCommitsApp.Controllers
 {
-    public class CommitExprorerController
+
+    [ApiController] //indica que es un controlador api
+    [Route("CommitExplorer")] // Ruta del controlador
+    public class CommitExprorerController : ControllerBase //Indicar que es un controlador
     {
+        
+        [HttpGet]
+        [Route("Prueba")]
+        public dynamic Ejemplo() 
+        {
+            return new 
+            { 
+                message = "Commit Explorer Aplications"
+            };
+        }
     }
 }
