30e939833c0346ae5d875a9d092773086e78def6
Jonnier Teran <jonnierteranmorales@gmail.com>
2024-02-02 17:36:34 -0500

    Implementacion de interfaz ICExprorerService En el ExplorerService

--- Services/CommitExplorerService.cs
+++ Services/CommitExplorerService.cs
@@ -1,8 +1,19 @@
-﻿namespace ExploradorCommitsApp.Services
+﻿using ExploradorCommitsApp.Models;
+
+namespace ExploradorCommitsApp.Services
 {
 
-    
-    public class CommitExplorerService 
+
+    public class CommitExplorerService : ICExplorerCommitsService
     {
+        public dynamic loadRepositories(string library)
+        {
+            throw new NotImplementedException();
+        }
+
+        public dynamic quantityCommits(string owner, string repositoryName)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
