5fbd6f1904e2fdffc5770d57c6cfd4abd3b3f1f3
Jonnier Teran <jonnierteranmorales@gmail.com>
2024-02-03 20:07:11 -0500

    Corrigiendo Accesibilidad de metodos en el servicio

--- Services/CommitExplorerService.cs
+++ Services/CommitExplorerService.cs
@@ -8,6 +8,9 @@ namespace ExploradorCommitsApp.Services
 {
     public class CommitExplorerService : IComitExplorerService
     {
+        /// <summary>
+        /// Propiedad para de tipo httpClient como servicio para peticiones http
+        /// </summary>
         private readonly HttpClient _httpClient;
 
         public CommitExplorerService()
@@ -28,21 +31,27 @@ namespace ExploradorCommitsApp.Services
         /// <returns> Lista de los commits</returns>
         public async Task<IActionResult> CommitsPorSemana(RequestCommits data)
         {
+            //Se crea un Arrays de Objetos que retornara el metodo
             List<ResponseCommits> responseData = new List<ResponseCommits>();
+
+            //Se Ejecuta el metodo asincrono y se obtienen los Repositorios que utilizan x tecnologia
             var Repo = await this.CommitsPorLibreria(data);
 
+            //Validacion de la respuesta del metodo
             if (Repo != null)
             {
-                if (Repo.items.Count > 0)
-                {
+                if (Repo.items.Count > 0)  // Se Realiza un conteo de la propiedad Items que contiene la informacion de los repositorios
+                {   
+                    //Se recorren los Repositorios segun la propiedad Items
                     for (int i = 0; i < Repo.items.Count; i++)
                     {
-                        ResponseCommits auxData = new ResponseCommits();
-                        var full_name = Repo.items[i].full_name;
-                        var da = await this.CommitsPorSemana2(full_name);
-                        auxData.infoRepo = da.Data;
-                        auxData.nameRepo = full_name;
-                        responseData.Add(auxData);
+                        
+                        ResponseCommits auxData = new ResponseCommits(); // Variable auxiliar para agregar a la lista
+                        var full_name = Repo.items[i].full_name; // Se Obtiene la propiedad full_name que contiene nombre/Repositorio para el Endpoint de github
+                        var info = await this.CommitsSemanales(full_name); //Por medio de un metodo asincrono se consultan los comits por semana de x repositorio
+                        auxData.infoRepo = info.Data;  // Se pasa la Data Obtenda (Cantidad de comits semanales) al objeto auxiliar
+                        auxData.nameRepo = full_name; // Se pasa el nombre del Repositorio
+                        responseData.Add(auxData); // Se agrega a la lista que se retornara como respuesta  
                     }
 
                 }
@@ -63,7 +72,9 @@ namespace ExploradorCommitsApp.Services
 
             try
             {
-                string apiUrl = $"https://api.github.com/search/repositories?q={data.libreria}&per_page={data.CantidadRepo}";            HttpResponseMessage response = await _httpClient.GetAsync(apiUrl);
+                //Consulta a la API De github por medio de este endpoint
+                string apiUrl = $"https://api.github.com/search/repositories?q={data.libreria}&per_page={data.CantidadRepo}";         
+                HttpResponseMessage response = await _httpClient.GetAsync(apiUrl);
 
                 if (response.IsSuccessStatusCode)
                 {
@@ -87,7 +98,6 @@ namespace ExploradorCommitsApp.Services
                 else
                 {
                     
-
                     return new RepositoryModels();
 
                 }
@@ -101,12 +111,16 @@ namespace ExploradorCommitsApp.Services
         }
 
         //-------------------------------------------------------------------------------------------------------------------------------------------
-        private  async Task<ResponseCommitUnit> CommitsPorSemana2(string fullName)
+        
+        
+        private  async Task<ResponseCommitUnit> CommitsSemanales(string fullName)
         {
-            var responseFinal = new ResponseCommitUnit();
+            
+            var responseFinal = new ResponseCommitUnit(); //Instancia para la respuesta
+
             try
             {
-
+                //Endpoint Para Consultar Commits Semanales por cada Repositorio
                 string apiUrl = "https://api.github.com/repos/" + fullName + "/stats/participation";
                 HttpResponseMessage response = await _httpClient.GetAsync(apiUrl);
 
@@ -119,11 +133,17 @@ namespace ExploradorCommitsApp.Services
                     var responseObject = JsonConvert.DeserializeObject<countCommitsModels>(responseBody);
 
                     // Devuelve un  con el objeto deserializado
+                    if(responseObject != null)
+                    {
                     responseFinal.Data = responseObject;
+                    return responseFinal;
+
+                    }
                     return responseFinal;
                 }
                 else
                 {
+                    //Manejo de Errores
                     responseFinal.MessageError = "No hay data para mostrar";
                     responseFinal.Data = null;
                     return responseFinal;
@@ -132,7 +152,7 @@ namespace ExploradorCommitsApp.Services
             }
             catch (Exception ex)
             {
-
+                //Manejo de Errores
                 responseFinal.MessageError = ex.Message;
                 responseFinal.Data = null;
                 return responseFinal;
