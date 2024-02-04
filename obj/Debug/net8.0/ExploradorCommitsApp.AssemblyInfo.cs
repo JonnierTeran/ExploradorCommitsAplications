173842d00c9fd1c449736eda86b1ff48e0153ec9
Jonnier Teran <jonnierteranmorales@gmail.com>
2024-02-04 10:05:33 -0500

    Actualizando  obj/Debug/net8.0/ExploradorCommitsApp.AssemblyInfo.cs

--- obj/Debug/net8.0/ExploradorCommitsApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/ExploradorCommitsApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExploradorCommitsApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4b39e9885caec213ebaeea715de13379883bf4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExploradorCommitsApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExploradorCommitsApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
